b41d2301bf0b59c169c5b3d1076617b89a946486
Valerino Wijaya <valerino@gmail.com>
2014-03-20 13:22:22 +0700

    Initial Commit

---
+++ Man's Best Friend/VWViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWViewController.h
+//  Man's Best Friend
+//
+//  Created by Valerino on 3/20/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWViewController : UIViewController
+
+@end
---
+++ Man's Best Friend/VWViewController.m
@@ -0,0 +1,29 @@
+//
+//  VWViewController.m
+//  Man's Best Friend
+//
+//  Created by Valerino on 3/20/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import "VWViewController.h"
+
+@interface VWViewController ()
+
+@end
+
+@implementation VWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
